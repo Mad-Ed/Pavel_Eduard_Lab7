39de58b8932dd707104b1a979ab03f42a9d06784
Mad-Ed <pavelcristianeduard22@yahoo.com>
2022-12-19 15:35:31 +0200

    b

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,9 @@ namespace Pavel_Eduard_Lab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
+
     }
 }
